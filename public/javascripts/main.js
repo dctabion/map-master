9f599d225be72a937691c05f2c2e815c48877190
Daniel Tabion <dctabion@gmail.com>
2018-01-17 16:00:23 -0600

    added jquery and main script

---
+++ public/javascripts/main.js
@@ -0,0 +1,30 @@
+window.onload = function() {
+
+  /* -------------- USER STARTS GAME ---------------*/
+  // Wait for user to start game
+
+  /* -------------- INITIALIZE GAME ----------------*/
+  // create an array of countries
+  // Play start game sound
+
+  // Play music
+  // Start timer
+
+  /* -------------- GAME LOOP ----------------------*/
+  // while (countriesNotDoneArray has item)
+    // select random country from countriesNotDoneArray
+    // get click
+    // if country clicked is in the countriesNotDoneArray
+    // if user is correct, move country to done-array & increase number of attempts
+    // make correct sound
+
+    // if wrong, increase number of attempts
+    // make incorrect sound
+};
+
+
+// {
+//   name:
+//   done:
+//   numberOfAttempts:
+// }
