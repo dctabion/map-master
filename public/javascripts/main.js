5a585252b5a99812d6439497fdd06f233a7a9ccb
Daniel Tabion <dctabion@gmail.com>
2018-01-17 16:28:31 -0600

    added styling for page, background image, mouse event to display country name

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -4,7 +4,55 @@ window.onload = function() {
   // Wait for user to start game
 
   /* -------------- INITIALIZE GAME ----------------*/
+  // Attach handler to display country
+  $(".land").hover(function(){
+    // console.log('click!');
+    // console.log('ID: ', this.id);
+    // console.log('class: ', $("#" +this.id).attr("class"));
+    // console.log('title: ', $("#" +this.id).attr("title"));
+
+    // Display country name when event occurs
+    document.getElementById('country-name').innerHTML = $("#" + this.id).attr("title");
+  });
+
   // create an array of countries
+  var countryPathsFromSVG = $(".land");
+  console.log("$(\".land\")", countryPathsFromSVG);
+  console.log('countryPathsFromSVG[4]: ', countryPathsFromSVG[4]);
+  console.log('-------shiznits-------');
+  console.dir(countryPathsFromSVG[4]);
+  console.log("ID:", countryPathsFromSVG[4].attributes.id.nodeValue);
+  console.log("class:", countryPathsFromSVG[4].attributes.class.nodeValue);
+  console.log("Country Name:", countryPathsFromSVG[4].attributes.title.nodeValue);
+
+  // var countries = [];
+
+  var createCountryObject = function(countryPath, i, arr) {
+    // console.log("countryPath: ", countryPath);
+    console.log('----countyPath-----');
+    console.dir(countryPath);
+    // DEBUG Why do I need this? Why is 'countryPath' only an index.  Shouldn't map pass in each item of the array?
+    var countryPathObj = countryPathsFromSVG[countryPath];
+    // console.log("countryPathObj:", countryPathObj);
+    //
+    var country = {};
+    country.id = countryPathObj.attributes.id.nodeValue;
+    country.name = countryPathObj.attributes.title.nodeValue;
+    country.attempts = 0;
+    // console.log("country: ", country);
+    return country;
+  };
+
+  console.log('countryPathsFromSVG[4]: ', countryPathsFromSVG[4]);
+  console.log('typeof(countryPathsFromSVG[4]): ', typeof(countryPathsFromSVG[4]));
+  console.log("typeof(countryPathsFromSVG): ", typeof(countryPathsFromSVG));
+  console.log("-----Yodelleheehooo------");
+  console.dir(countryPathsFromSVG[4]);
+  var countries;
+  countries = countryPathsFromSVG.map(createCountryObject);
+  // console.log(countries);
+  console.log("countries[3]: ", countries[3]);
+
   // Play start game sound
 
   // Play music
